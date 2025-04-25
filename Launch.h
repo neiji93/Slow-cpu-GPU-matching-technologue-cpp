25a9930c3ee3f9c5af2adeb763cb9d03f36f8d78
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-25 07:49:50 +0200

    Create Launch.h

---
+++ Launch.h
@@ -0,0 +1,5 @@
+//may ne weong.
+//as we deal with equatioks and as we focus on coeffixient hear, we will have to
+//check for hashkel transforms. the behaviour of the initial patching program
+//would change..and we can still use devixes or drop.this latest point.
+//so we deal wirh models and equatkons.
