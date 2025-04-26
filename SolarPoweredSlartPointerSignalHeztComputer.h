7242900ea9cd7f24bc9529c4b553d433e16a0a08
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-26 10:58:42 +0200

    Update SolarPoweredSlartPointerSignalHeztComputer.h

--- SolarPoweredSlartPointerSignalHeztComputer.h
+++ SolarPoweredSlartPointerSignalHeztComputer.h
@@ -1,3 +1,16 @@
 //componenet shave to be enlighted by luminous power.
 //the initial value is not defined but must be in a wide range of eigen values.
 //
+
+
+class patchprobeinstantalgorithm
+{
+public:
+  void preprocess()
+{
+  //wait 10^15 patches of zny or multiple programd
+  //before any operations. this value is correct if we take a look
+  //at rrlationships of conduction and convection heat transfer.
+  //the value is based on mpi prepa coirses (french school)
+}
+}
